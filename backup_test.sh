db490f78dd2e425ee493f3162e89951da9ac127c
marymaksemos <117455640+marymaksemos@users.noreply.github.com>
2023-01-13 19:35:00 +0100

    the second commit
    "This script is for editing remote targets and creating incremental backups, after I make changes to it."
    "I tested this with both local and remote targets, and it works as intended."

--- backup_test.sh
+++ backup_test.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "Error: two arguments are required - the directory to be backed up, remote server IP and the destination directory for the backup archive."
-    exit 1
+   echo "Error: two arguments are required - the directory to be backed up, remote server IP and the destination directory for the backup archive."
+   exit 1
 fi
 
-backup_dir=$1
-dest=$2
+source_dir=$1
+target_dir=$2
 
-if [ ! -d "$backup_dir" ]; then
-    echo "Error: The directory $backup_dir does not exist."
-    exit 1
-fi
+if [[ $target_dir == *":"* ]]; then
+  # Use rsync over ssh for remote targets
+  server_and_dir=$target_dir
+  server=$(echo $server_and_dir | awk -F':' '{print $1}')
+  remote_dir=$(echo $server_and_dir | awk -F':' '{print $2}')
+ 
+  timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+  backup_dir="$remote_dir/$timestamp"
+  ssh $server "mkdir $backup_dir"
 
-if [[ $dest == *":"* ]]; then
-    rsync -avz -e ssh $bachup_dir $dest
-    date=$(date +%Y-%m-%d)
-    backup_name="backup-$date.tar.gz"
-    tar -zcf "$dest/$backup_name" -C "$backup_dir" .
+  rsync -avz -e "ssh -T" --link-dest=$server:$remote_dir/latest $source_dir $server:$backup_dir
+
+# Update the symlink for the latest backup
+  ssh $server "ln -snf $backup_dir $remote_dir/latest"
 
 else
-    if [ ! -d "$dest" ]; then
-        echo "Error: The directory $dest does not exist."
-        exit 1
-    fi
+  # Use local rsync for local targets 
 
-    date=$(date +%Y-%m-%d)
-    backup_name="backup-$date.tar.gz"
-    tar -zcf "$dest/$backup_name" -C "$backup_dir" .
+ timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+ backup_dir="$target_dir/$timestamp"
+
+ mkdir $backup_dir
+
+ rsync -avz --link-dest=$target_dir/latest $source_dir $backup_dir
+
+# Update the symlink for the latest backup
+ ln -snf $backup_dir $target_dir/latest
 fi
-
-echo "Backup complete: $dest"
